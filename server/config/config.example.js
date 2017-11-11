1429bdd11bf1207304422670763254bf731947a4
Reid Mollway <reidmollway@gmail.com>
2017-11-10 21:07:36 -0500

    Added ref to DB

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -1,13 +1,10 @@
-//This file holds any configuration variables we may need 
+//This file holds any configuration variables we may need
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
 module.exports = {
   db: {
-    uri: '', //place the URI of your mongo database here.
-  }, 
-  googleMaps: {
-    key: ''
+    uri: 'mongodb://test:test@ds259255.mlab.com:59255/winstonsports', //place the URI of your mongo database here.
   },
-  port: 8080
+  port: process.env.PORT;
 };
